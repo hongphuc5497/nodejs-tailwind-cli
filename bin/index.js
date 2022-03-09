15b482cb3048e53d74c4c39938be0fa3d887a2e5
Hong Phuc <hongphuc.dthp@gmail.com>
2022-03-10 01:12:35 +0700

    adding logic code with shell

--- bin/index.js
+++ bin/index.js
@@ -1,25 +1,24 @@
 #! /usr/bin/env node
 
 const inquirer = require('inquirer');
-
-const questions = [
-	{
-		type: 'list',
-		name: 'framework',
-		message: 'Which framework do you want to use?',
-		choices: ['React', 'VueJS', 'Angular', 'NextJS', 'Svelte'],
-	},
-];
-
-const questionsTs = [
-	{
-		type: 'list',
-		name: 'typescript',
-		message: 'Does your project use TypeScript?',
-		choices: ['Yes', 'No'],
-	},
-];
+const shell = require('shelljs');
+const path = process.cwd();
+const { links, questions, questionsTs } = require('./utils');
+console.log(links.get('React'))
 
 inquirer.prompt(questions).then((answers) => {
-	console.log(answers);
+  const { framework, projectName } = answers;
+  console.log(framework, projectName)
+
+	shell.exec(`mkdir ${projectName}`);
+	console.log('📁 Created a folder for the project');
+
+	shell.exec(`git clone ${links.get(framework)} ${projectName}`);
+	console.log(`🖨️  Cloned started files into ${projectName}`);
+
+	shell.cd(`${path}/${projectName}`);
+	shell.exec('npm install');
+	console.log(
+		'👨‍💻  Successfully installed all the required dependencies\nHappy hacking 🚀'
+  );
 });
