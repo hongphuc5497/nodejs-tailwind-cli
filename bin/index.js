80d582785bbc9d43ed6bb46dec844c98e911ae25
Hong Phuc <hongphuc.dthp@gmail.com>
2022-03-09 23:34:53 +0700

    add interactive CLI interface with Inquirer

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,25 @@
 #! /usr/bin/env node
 
+const inquirer = require('inquirer');
 
+const questions = [
+	{
+		type: 'list',
+		name: 'framework',
+		message: 'Which framework do you want to use?',
+		choices: ['React', 'VueJS', 'Angular', 'NextJS', 'Svelte'],
+	},
+];
+
+const questionsTs = [
+	{
+		type: 'list',
+		name: 'typescript',
+		message: 'Does your project use TypeScript?',
+		choices: ['Yes', 'No'],
+	},
+];
+
+inquirer.prompt(questions).then((answers) => {
+	console.log(answers);
+});
